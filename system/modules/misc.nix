c3a270326a6e3e62a5d605bf7f1c454fb7009318
inlinefun <inlinefun@proton.me>
2025-10-11 14:45:07 +0530

    add: needed "xdg-desktop-portal"s

--- system/modules/misc.nix
+++ system/modules/misc.nix
@@ -26,4 +26,14 @@
     ];
   };
 
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gnome
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-hyprland
+    ];
+  };
+
 }
