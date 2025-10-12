cd9fead4ecd2dbf8280bff3a49d193e37142d24c
inlinefun <inlinefun@proton.me>
2025-10-12 13:20:53 +0530

    add: home-manager

--- flake.nix
+++ flake.nix
@@ -3,12 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { self, nixpkgs, ... }@inputs:
-    let 
+    {
+      self,
+      home-manager,
+      nixpkgs,
+      ...
+    }@inputs:
+    let
       arch = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${arch};
     in
     {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
@@ -17,6 +27,14 @@
         specialArgs = { inherit inputs; };
         modules = [
           ./system
+          home-manager.nixosModules.default
+        ];
+      };
+      homeConfigurations.me = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        extraSpecialArgs = { inherit inputs; };
+        modules = [
+          ./home
         ];
       };
     };
---
+++ home/home.nix
@@ -0,0 +1,24 @@
+{ ... }:
+
+let
+  user = "me";
+in
+{
+  home = {
+    username = "${user}";
+    homeDirectory = "/home/${user}";
+
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "25.05";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+}
