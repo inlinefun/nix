bf67433e37b3588b1ead773171382fc83fe87fab
inlinefun <inlinefun@proton.me>
2026-01-10 18:07:17 +0530

    update: hyprland, add: chezmoi

--- system/programs/hyprland.nix
+++ system/programs/hyprland.nix
@@ -2,13 +2,36 @@
 
 let
   packages = with pkgs; [
+    bibata-cursors
+    hyprcursor
+    hyprland-qt-support
+    hyprland-protocols
+    hyprlock
+    hyprpaper
+    hyprpolkitagent
     kitty
+    matugen
+    mako
+    rofi
   ];
 in
 {
   environment.systemPackages = packages;
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
+  programs = {
+    hyprland = {
+      enable = true;
+      xwayland.enable = true;
+    };
+    starship.enable = true;
+    uwsm = {
+      enable = true;
+      waylandCompositors = {
+        hyprland = {
+          prettyName = "Hyprland";
+          comment = "Hyprland compositor managed by UWSM";
+          binPath = "/run/current-system/sw/bin/start-hyprland";
+        };
+      };
+    };
   };
 }
