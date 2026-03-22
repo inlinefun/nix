5e23e39f7bf2eedf447c14ec18938cfbcfedd920
inlinefun <inlinefun@proton.me>
2026-03-22 16:58:47 +0530

    update(packages): add qmlls to $PATH

--- hosts/x515/packages/extras.nix
+++ hosts/x515/packages/extras.nix
@@ -1,10 +1,17 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    nil
-    nixd
-    nixfmt
-    qt6Packages.qtlanguageserver
-  ];
+  environment.systemPackages =
+    with pkgs;
+    [
+      nil
+      nixd
+      nixfmt
+    ]
+    ++ [
+      qt6Packages.qtbase
+      qt6Packages.qtdeclarative
+      qt6Packages.qtlanguageserver
+      qt6Packages.qtsvg
+    ];
 }
