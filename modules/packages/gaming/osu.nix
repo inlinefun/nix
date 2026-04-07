3bc65dd9c77fbc55574a44fbc585f4012b035965
inlinefun <inlinefun@proton.me>
2026-04-07 16:13:33 +0530

    update(osu!lazer): package options

--- modules/packages/gaming/osu.nix
+++ modules/packages/gaming/osu.nix
@@ -7,6 +7,9 @@
 
 let
   cfg = config.packages.osu;
+  package = pkgs.osu-lazer-bin.override {
+    nativeWayland = cfg.wayland;
+  };
 in
 {
 
@@ -14,12 +17,16 @@ in
     enable = lib.mkEnableOption ''
       Whether to enable osu!, A freeware rhythm game
     '';
+    wayland = lib.mkEnableOption ''
+      Whether to ask the SDL backend to use wayland
+    '';
     package = lib.mkOption {
-      default = pkgs.osu-lazer-bin;
+      default = package;
       description = ''
         Specify which package of osu! to install
         Defaults to osu!lazer's AppImage build for multiplayer and score submission
       '';
+      type = lib.types.package;
     };
   };
 
