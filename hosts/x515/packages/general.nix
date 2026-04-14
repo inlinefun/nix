0e6f5488418ccf2f2524eef847c5be69920bdfc7
inlinefun <inlinefun@proton.me>
2026-04-14 12:58:18 +0530

    update(packages): quickshell dependencies

--- hosts/x515/packages/general.nix
+++ hosts/x515/packages/general.nix
@@ -16,7 +16,17 @@
     };
     quickshell = {
       enable = true;
-      package = inputs.quickshell.packages."x86_64-linux".default;
+      package = inputs.quickshell.packages."x86_64-linux".default.withModules [
+        pkgs.qt6Packages.qt3d
+        pkgs.qt6Packages.qt5compat
+        pkgs.qt6Packages.qtsvg
+        pkgs.qt6Packages.qtimageformats
+        pkgs.qt6Packages.qtmultimedia
+        pkgs.qt6Packages.qtquick3d
+        pkgs.qt6Packages.qtquick3dphysics
+        pkgs.qt6Packages.qttools
+        pkgs.kdePackages.qtquicktimeline
+      ];
     };
   };
 
